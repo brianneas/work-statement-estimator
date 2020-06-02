7c8a09c4011bb6205d49248e2f629aa7219cd341
Rebelpebble <neasbrian@gmail.com>
2020-06-02 18:53:23 -0500

    Add drop down options for part type and complexity

--- client/app.js
+++ client/app.js
@@ -1,12 +1,30 @@
 function addRow() {
    const newRow = $('<tr></tr>')
-   const cellsInRow = 3
 
-   for (i = 0; i < cellsInRow; i++) {
-      const newCell = $('<td></td>')
-      newCell.attr("contenteditable", true)
-      newRow.append(newCell)
-   }
+   // Create text cell to input part number
+   const newCell = $('<td></td>')
+   newCell.attr("contenteditable", true)
+   newRow.append(newCell)
+
+   // add part type options
+   const partOptions = ['Detail', 'Assembly', 'Module']
+   const partSelection = $('<select></select>')
+
+   partOptions.forEach((partType, i) => {
+      partSelection.append($('<option></option>').text(partType))
+   })
+
+   newRow.append($('<td></td>').append(partSelection))
+
+   // add complexity options
+   const complexityOptions = ['Simple', 'Average', 'Complex']
+   const complexitySelection = $('<select></select>')
+
+   complexityOptions.forEach((complexityType, i) => {
+      complexitySelection.append($('<option></option>').text(complexityType))
+   })
+
+   newRow.append($('<td></td>').append(complexitySelection))
 
    $("#table").append(newRow)
 }
