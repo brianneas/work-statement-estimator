affb378b23ca6f8810e4b332707a9098f991d87b
Rebelpebble <neasbrian@gmail.com>
2020-06-05 11:19:32 -0500

    Broke out enable and disable edit mode into separate functions

--- client/app.js
+++ client/app.js
@@ -81,40 +81,48 @@ function renumberButtons() {
    })
 }
 
+function enableEditMode() {
+   const tableRows = $("#table").find('tr')
+
+   tableRows.find("th:first-child").remove()
+   tableRows.find("td:first-child").remove()
+
+   $('#addButton').prop("disabled", false)
+   $('#deleteButton').prop("disabled", false)
+
+   $('#table').removeClass('editMode')
+}
+
+function disableEditMode() {
+   $('tr.' + rowClass).each(function(index) {
+      if (index === 0) {
+         $(this).prepend('<th>Remove</th>')
+      } else {
+         // add button with an id identifying the row it is on
+         const button = $('<button>X</button>').click(function() {
+            const thisButton = this
+            removeCurrentRow(thisButton)
+         })
+
+         button.attr('id', buttonIdPrefix + index)
+         button.addClass(editButtonClass)
+         $(this).prepend($('<td></td>').append(button))
+
+         // add a row id to each row
+         $(this).attr('id', rowIdPrefix + index)
+      }
+   })
+
+   $('#addButton').prop("disabled", true)
+   $('#deleteButton').prop("disabled", true)
+
+   $('#table').addClass('editMode')
+}
+
 function editRows() {
    if ($('#table').hasClass('editMode')) {
-      const tableRows = $("#table").find('tr')
-
-      tableRows.find("th:first-child").remove()
-      tableRows.find("td:first-child").remove()
-
-      $('#addButton').prop("disabled", false)
-      $('#deleteButton').prop("disabled", false)
-
-      $('#table').removeClass('editMode')
+      enableEditMode()
    } else {
-      $('tr.' + rowClass).each(function(index) {
-         if (index === 0) {
-            $(this).prepend('<th>Remove</th>')
-         } else {
-            // add button with an id identifying the row it is on
-            const button = $('<button>X</button>').click(function() {
-               const thisButton = this
-               removeCurrentRow(thisButton)
-            })
-
-            button.attr('id', buttonIdPrefix + index)
-            button.addClass(editButtonClass)
-            $(this).prepend($('<td></td>').append(button))
-
-            // add a row id to each row
-            $(this).attr('id', rowIdPrefix + index)
-         }
-      })
-
-      $('#addButton').prop("disabled", true)
-      $('#deleteButton').prop("disabled", true)
-
-      $('#table').addClass('editMode')
+      disableEditMode()
    }
 }
